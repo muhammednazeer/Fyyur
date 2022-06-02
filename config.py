49ea4cbc809347f23df321a2fe1fd55a52cf11d0
muhammednazeer <nazeer4u2c@gmail.com>
2022-06-02 13:04:58 +0100

    insert database URI in the config file

--- config.py
+++ config.py
@@ -9,5 +9,6 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# DATABASE CONNECTION STRING
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Proton45@localhost:5432/fyyurDB'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
